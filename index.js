5f059c81cdebdefec5bec270fb70c2791eab7ce9
SonyaCode <sonyaz2@nycstudents.net>
2024-10-18 10:58:45 -0400

    Initial Commit

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
 // Build a BlackJack Game
-
+let cards = [];
+let hasBlackjack = false;
+let isAlive;
+let message;
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el");
 // variables representing two cards 
-let firstCard = 10
-let secondCard = 4
-
-//challenge #1
-//create a new variable and set it to the sum of the two cards 
+let firstCard;
+let secondCard;
+let sum;
 
 
-//chalenge #2 
-// code the statements below based on the conditions shown in blackjack.png from the images folder
-// your output should work in the browser's console based on changing the values assigned to the cards
+function renderGame() {
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += " " + cards[i]
+    }
 
-//starter code
-    message = "Do you want to draw a new card? 🙂"
-    message = "Wohoo! You've got Blackjack! 🥳"
-    message = "You're out of the game! 😭"
-console.log(message)
+    sum = firstCard + secondCard
+    sumEl.innerHTML = sum;
+
+    if (sum < 21) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum == 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+    } else {
+        message = "You're out of the game! 😭"
+    }
+
+    messageEl.innerText = message;
+}
+
+function newCard() {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+}
+
+function startGame() {
+    isAlive = true;
+    firstCard = getRandomCard();
+    secondCard = getRandomCard();
+    cards.push(firstCard);
+    cards.push(secondCard);
+    renderGame();
+}
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13 + 1);
+
+    if (randomNumber == 1) {
+        return 11
+    } else if (randomNumber > 9) {
+        return 10
+    } else {
+        return randomNumber
+    }
+}
